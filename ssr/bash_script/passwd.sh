1db0933864f26df441fee5949f67de7df43c883c
NJ2046 <582492559@qq.com>
2019-10-17 09:43:26 +0800

    Update passwd.sh

--- ssr/bash_script/passwd.sh
+++ ssr/bash_script/passwd.sh
@@ -1,3 +1,9 @@
 sudo yum install -y wget
 sudo wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
 sudo bash ./bbr.sh
+
+ /etc/v2ray/config.json
+ service v2ray restart
+ 
+ /etc/shadowsocks.json
+ /etc/init.d/shadowsocks start
